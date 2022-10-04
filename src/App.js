026c91f99e191549dfc6a1524bd860e055f28360
Moaz A.Moshtha <moazmoshtha@gmail.com>
2022-10-04 18:42:09 +0300

    import ItemView

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "monday-ui-react-core/dist/main.css"
+import "./App.css";
+import ItemView from "./components/ItemView";
+
+const App = () => {
+    return (
+        <div className="App">
+            <ItemView/>
+        </div>
+    );
+};
+
+export default App;
